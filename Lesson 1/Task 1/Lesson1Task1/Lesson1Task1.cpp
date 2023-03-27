301e9ce42d31c89b6b42a50cf71d2cc6c062fa95
Elena An <moore.helena@yandex.ru>
2023-03-27 23:34:05 +0300

    add lesson 1 homeworks

---
+++ Lesson 1/Task 1/Lesson1Task1/Lesson1Task1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+void client(int &counter, int max)
+{
+    while (counter < max)
+    {
+        ++counter;
+        std::cout << "Клиентов в очереди: " << counter << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+void operationist(int &counter)
+{
+    while (counter > 0)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        --counter;
+        std::cout << "Осталось клиентов: " << counter << std::endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int counter = 0, max = 10;
+
+    std::thread t1(client, std::ref(counter), max);
+    std::thread t2(operationist, std::ref(counter));
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ Lesson 1/Task 2/Lesson1Task2/Lesson1Task2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+void sum(std::vector<int> v1, std::vector<int> v2, std::vector<int> v_sum, int core_number, int thread_number)
+{
+    size_t i = 0, j = 0;
+
+    if (core_number == 1 || thread_number == 1)
+    {
+        j = v1.size() / core_number;
+    }
+    else if (core_number == thread_number)
+    {
+        i = v1.size() / core_number * (thread_number - 1);
+        j = v1.size();
+    }
+    else {
+        i = v1.size() / core_number * (thread_number - 1);
+        j = v1.size() / core_number * thread_number;
+    }
+    
+    for (i; i < j; ++i)
+    {
+        v_sum[i] = v1[i] + v2[i];
+    }
+}
+
+double time(std::vector<int> v1, std::vector<int> v2, std::vector<int> v_sum, int core_number)
+{
+    auto start = std::chrono::steady_clock::now();
+    std::vector<std::thread> v_thread;
+    for (int i = 1; i <= core_number; ++i)
+    {
+        v_thread.push_back(std::thread(sum, v1, v2, v_sum, core_number, i));
+    }
+    for (auto& t : v_thread)
+    {
+        t.join();
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    return duration.count();
+}
+
+void execution(int core_number)
+{
+    std::cout << core_number << " потоков\t";
+    
+    for (int i = 1000; i <= 1000000; i *= 10)
+    {
+        std::vector<int> v1(i, 1);
+        std::vector<int> v2(i, 1);
+        std::vector<int> v_sum(i, 0);
+
+        std::cout << time(v1, v2, v_sum, core_number) << "s\t";
+    }
+
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t\t" << "1000\t\t" << "10000\t\t" << "100000\t\t" << "1000000\t" << std::endl;
+
+    for (int i = 1; i <= 16; i *= 2)
+    {
+        execution(i);
+    }
+
+    return 0;
+}
