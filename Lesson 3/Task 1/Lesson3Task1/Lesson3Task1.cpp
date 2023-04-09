2198059b40907267c54bc6ca715b9ed84e2d0ba6
Elena An <moore.helena@yandex.ru>
2023-04-09 11:44:16 +0300

    add lesson 3 homeworks

---
+++ Lesson 3/Task 1/Lesson3Task1/Lesson3Task1.cpp
@@ -0,0 +1,49 @@
+﻿#include <future>
+#include <iostream>
+
+void findMinimum(int* arr, int size, int index, std::promise<int> pr)
+{
+	int actual_index = index;
+
+	for (int i = index; i < size; ++i)
+	{
+		if (arr[i] < arr[actual_index])
+		{
+			actual_index = i;
+		}
+	}
+
+	pr.set_value(actual_index);
+}
+
+void asyncSort(int* arr, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::promise<int> pr;
+		std::future<int> ft = pr.get_future();
+		auto async = std::async(findMinimum, arr, size, i, std::move(pr));
+		ft.wait();
+		int result = ft.get();
+
+		if (result > i)
+		{
+			int temp = arr[i];
+			arr[i] = arr[result];
+			arr[result] = temp;
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 1, -5, 23, 60, -8, 10, 0, 10, 2, 42 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	asyncSort(arr, size);
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
---
+++ Lesson 3/Task 2/Lesson3Task2/Lesson3Task2.cpp
@@ -0,0 +1,51 @@
+﻿#include <future>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+template<typename Iterator, typename T>
+void parallelForEach(Iterator first, Iterator last, T* function)
+{
+    unsigned int const length = std::distance(first, last);
+    unsigned int const max = 1;
+ 
+
+    if (length <= max) 
+    {
+        function(*first);
+    }
+    else
+    {
+        Iterator middle = first;
+        std::advance(middle, length / 2);
+        std::future<void> first_half_result = std::async(parallelForEach<Iterator, T>, first, middle, function);
+        std::future<void> second_half_result = std::async(parallelForEach<Iterator, T>, middle, last, function);
+    }
+}
+
+void printX(int x) 
+{   
+    std::string result = std::to_string(x) + " ";
+    std::cout << result;
+}
+
+int main()
+{
+    std::vector<int> v;
+    int n = 100;
+    for (int i = 0; i < n; ++i)
+    {
+        v.push_back(i);
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    parallelForEach(v.begin(), v.end(), &printX);
+
+    return 0;
+}
